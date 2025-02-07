83f602d1a2d84f00289fa81e94441ce694f67724
Aryan Rakesh Mane <aryanmane890000@gmail.com>
2025-02-07 09:14:59 +0530

    -

---
+++ backend-app/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.post("/user/signup", function(req, res){
+	json.send({
+		message: "signup endpoint"
+	})
+})
+
+app.post("/user/signin	", function(req, res){
+
+})
+
+
+app.post("/user/purchases", function(req, res){
+
+})
+
+app.post("/course/purchase", function(req, res){
+
+})
+app.post("/courses", function(req, res){
+
+})
+
+
+app.listen(3000);
